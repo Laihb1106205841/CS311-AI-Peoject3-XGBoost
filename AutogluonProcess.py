443a78a6ebff1f5f1a09d470ee44778f890acbf9
admin <12211612@mail.sustech.edu.cn>
2024-06-08 18:13:09 +0800

    Running AutogluonProcess.ipynb

--- AutogluonProcess.py
+++ AutogluonProcess.py
@@ -1,19 +1,18 @@
-
+###########################################################################
 # Try to implement different Model on the dataset by using autogluon
 
 # 如果你没有看AutogluonProcess.ipynb，我推荐你先去看那个文件里的文档
 # please look at file AutogluonProcess.ipynb first
+###########################################################################
 
 import autogluon as ag
 import pandas as pd
 from autogluon.tabular import TabularDataset
-from tensorboardX import SummaryWriter
+
 import warnings
 warnings.filterwarnings('ignore')
-import matplotlib.pyplot as plt
-import numpy as np
 
-
+# -------------------------- Preprocessing ----------------------------- #
 # 读取 train.csv 文件
 train_df = pd.read_csv("data/traindata.csv")
 
@@ -24,16 +23,14 @@ data_df = pd.read_csv("data/trainlabel.csv")
 train_df['Income'] = data_df['Income']
 
 # 将修改后的 train_df 保存为新的 CSV 文件
-train_df.to_csv("data/updated_train.csv", index=False)
+train_df.to_csv("data/traindata_preprocessed.csv", index=False)
 
-#
-#
 # loading
-train_data = pd.read_csv('data/updated_train.csv')
+train_data = pd.read_csv('data/traindata_preprocessed.csv')
 test_data = pd.read_csv('data/testdata.csv')
 
-test_data.head()
 
+# ---------------------------- Training ------------------------------ #
 label_column = 'Income'
 
 # training
@@ -42,10 +39,20 @@ predictor = ag.tabular.TabularPredictor(label=label_column, problem_type='binary
 
 predictor.fit(train_data=train_data)
 
+
+# ---------------------------- Training Result ----------------------- #
+results = predictor.fit_summary(show_plot=True)
+# leaderboard = predictor.leaderboard(test_data)   #预测结果看板
+lboard = predictor.leaderboard()
+print(lboard)
+
+
+# ---------------------------- Predicting ---------------------------- #
 # predicting
 performance = predictor.predict(test_data)
 print(performance)
 
-results = predictor.fit_summary(show_plot=True)
-# leaderboard = predictor.leaderboard(test_data)   #预测结果看板
-lboard = predictor.leaderboard()
+# 将结果保存到 CSV 文件
+performance.to_csv('predict/performance_predictions.csv', index=False)
+
+print("Predictions saved successfully to 'predict/performance_predictions.csv'")
