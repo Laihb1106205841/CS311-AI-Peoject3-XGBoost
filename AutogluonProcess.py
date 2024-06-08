fc65b7b192fb9490bf2f07b15844b0bc2b466e93
admin <12211612@mail.sustech.edu.cn>
2024-06-08 18:29:52 +0800

    Finishing AutogluonProcess.py Uploading AutogluonLog.txt

--- AutogluonProcess.py
+++ AutogluonProcess.py
@@ -3,6 +3,11 @@
 
 # 如果你没有看AutogluonProcess.ipynb，我推荐你先去看那个文件里的文档
 # please look at file AutogluonProcess.ipynb first
+
+# Input:    data/traindata.csv, data/trainlabel.txt
+# Output:   data/traindata_preprocessed.csv predict/Autogluon_predictions.csv
+# Running logs:
+# /logs/AutoluonLog.txt
 ###########################################################################
 
 import autogluon as ag
@@ -16,11 +21,23 @@ warnings.filterwarnings('ignore')
 # 读取 train.csv 文件
 train_df = pd.read_csv("data/traindata.csv")
 
+# 读取trainlabel.txt文件
+with open('data/trainlabel.txt', 'r') as file:
+    lines = file.readlines()
+
+# 解析文本数据并创建DataFrame
+data = {'Label': [line.strip() for line in lines]}
+df = pd.DataFrame(data)
+
+# 将DataFrame保存为CSV文件
+df.to_csv('data/trainlabel.csv', index=False)
+
+print("trainlabel.txt 已成功转换为 trainlabel.csv")
 # 读取 data.csv 文件，假设数据列名为 'income'
 data_df = pd.read_csv("data/trainlabel.csv")
 
 # 将 'income' 数据列添加到 train_df 中
-train_df['Income'] = data_df['Income']
+train_df['Income'] = data_df
 
 # 将修改后的 train_df 保存为新的 CSV 文件
 train_df.to_csv("data/traindata_preprocessed.csv", index=False)
@@ -53,6 +70,6 @@ performance = predictor.predict(test_data)
 print(performance)
 
 # 将结果保存到 CSV 文件
-performance.to_csv('predict/performance_predictions.csv', index=False)
+performance.to_csv('predict/Autogluon_predictions.csv', index=False)
 
-print("Predictions saved successfully to 'predict/performance_predictions.csv'")
+print("Predictions saved successfully to 'predict/Autogluon_predictions.csv'")
